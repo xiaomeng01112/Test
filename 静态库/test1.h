29408c2a9e3151f4493cec567547be2a10d30bf0
xincc.wang <willxincc@163.com>
2016-04-06 23:19:42 +0800

    first commit

---
+++ 静态库/test1.h
@@ -0,0 +1,14 @@
+//
+//  test1.h
+//  test1
+//
+//  Created by Wqiang on 16/4/5.
+//  Copyright © 2016年 reapal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test1 : NSObject
+
++ (NSString *)name;
+@end
---
+++ 静态库/test2.h
@@ -0,0 +1,13 @@
+//
+//  test2.h
+//  test2
+//
+//  Created by Wqiang on 16/4/5.
+//  Copyright © 2016年 reapal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test2 : NSObject
++ (NSString *)name;
+@end
